5b7b7c419f1b87f4e5d5247737925fc728d52670
Paseka Mabitsela <pamabswgc025@student.wethinkcode.co.za>
2025-12-10 18:32:02 +0200

    CRITICAL FIX: Clear service worker cache causing CORS errors

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+// Empty service worker to clear cache
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppSimple from './AppSimple.jsx'
 
+// Unregister any existing service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppSimple />
